a7badc0a7b6d0e7140505694e016a15d8868bf29
MrDeveloper02 <152008305+MrDeveloper02@users.noreply.github.com>
2024-12-03 12:32:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,28 +38,6 @@ function firstPageAnimtion(){
     })
 }
 
-// function cursorSkew(){
-//     //define default scale value 
-//     var xScale = 1;
-//     var yScale =1;
-
-//     //define previous position 
-//     var xPrev = 0;
-//     var yPrev = 0;
-
-//     window.addEventListener("mousemove",function(dets){
-//         yScale =  gsap.utils.clamp(.8,1,2,yDiff);
-//         xScale= gsap.utils.clamp(.8,1,2,xDiff);//gsap.utils.clamp is a function that clamps or sets any value other than the the given range to the nearest minima or maxima , for eg : if xDiff comes out to be 42 the its value gets sets to 1.2
-//         var xDiff = dets.clientX - xPrev;//this calculates the diff by subtracting the initial position from new position
-//         var yDiff = dets.clientY - yPrev;
-
-//         xPrev = dets.clientX;//initial positon of cursor in x-axis gets saved in x prev
-//         yPrev = dets.clientY;//initial positon of cursor in y-axis gets saved in y prev
-//      })
-
-
-
-// }
 
 
 mouseFollower();
